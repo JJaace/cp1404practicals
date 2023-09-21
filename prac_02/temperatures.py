f8fa8219d4abb5ce37467fe52dd663982d4e014f
Jaace <jaace.bani@my.jcu.edu.au>
2023-09-21 12:50:50 +1000

    Add temperature converter program

---
+++ prac_02/temperatures.py
@@ -0,0 +1,32 @@
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = convert_to_fahrenheit()
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            celsius = convert_to_celsius()
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+def convert_to_celsius():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+main()
