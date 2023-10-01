e23756e75174f8dd1f6a4d8ccf5b43ed70a95088
Jaace <jaace.bani@my.jcu.edu.au>
2023-10-01 23:28:26 +1000

    Add stock-price simulator

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -35,9 +35,9 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
 
-
     number_of_days += 1
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:.2f}", file=out_file.write())
+    print(f"On day {number_of_days} price is: ${price:.2f}")
     out_file.write(f"On day {number_of_days} price is: ${price:.2f}\n")
-    out_file.close()
+
+out_file.close()
