70ebf8d89a9fe8c5752f598d22fa73527d369c6d
Jaace <jaace.bani@my.jcu.edu.au>
2023-10-12 00:26:05 +1000

    Complete prac 5 hex colours exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,15 @@
+COLOUR_DICTIONARY = {"Absolutezero": "#0048ba", "Acidgreen": "#b0bf1a", "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                     "Aqua": "#00ffff", "Ashgrey": "#b2beb5", "Beaver": "#9f8170", "Beige": "#f5f5dc",
+                     "Bistre": "#3d2b1f",
+                     "Blond": "#faf0be"}
+
+for colour in COLOUR_DICTIONARY:
+    print(colour)
+
+colour = input("Enter Colour: ").title()
+while colour != "":
+    if colour in COLOUR_DICTIONARY:
+        print(f"{colour} has the colour code {COLOUR_DICTIONARY[colour]}")
+    else:
+        print("Invalid colour")
+    colour = input("Enter Colour: ").title()
