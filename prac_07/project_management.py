0999e88ee669c9c7142e8f7b18d5e4d669bc0471
Jaace <jaace.bani@my.jcu.edu.au>
2023-11-02 11:27:51 +1000

    Add load_project function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,3 +1,5 @@
+"""Estimate: 5 hours"""
+
 """Priority Queue
 1 - Load projects
 2 - Save Projects
@@ -6,6 +8,8 @@
 5 - filter project by date
 6 - Update project"""
 
+from prac_07.project import Project
+
 MENU = ("- (L)oad projects\n"
         "- (S)ave projects\n"
         "- (D)isplay projects\n"
@@ -13,12 +17,15 @@ MENU = ("- (L)oad projects\n"
         "- (A)dd new project\n"
         "- (Q)uit")
 
+projects = []
+
 user_input = ""
 while user_input != "Q":
     print(MENU)
     user_input = input(">>> ").upper()
     if user_input == "L":
-        print("Load")
+        filename = input("Enter filename to load projects from: ") + ".txt"
+        projects = Project.load_projects(filename)
     elif user_input == "S":
         print("Save")
     elif user_input == "D":
@@ -27,6 +34,8 @@ while user_input != "Q":
         print("Filter")
     elif user_input == "A":
         print("Add")
+    elif user_input == "U":
+        print("Update")
     elif user_input != "Q":
         print("Invalid Input")
 
